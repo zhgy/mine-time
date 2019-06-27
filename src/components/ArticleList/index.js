fb1b0035a2925df84913703f05803efe4edab2f4
Blunt <zhgy@hotmail.com>
2019-06-27 22:38:38 +0800

    Added style for ArticleList.

--- src/components/ArticleList/index.js
+++ src/components/ArticleList/index.js
@@ -1,41 +1,41 @@
 import React from 'react';
 
+import { StartBar } from '../StartBar';
 import './style.css';
 
-const ArticleListItem = props => {
+const ArticleItem = props => {
     return (
-        <div className="">
-            <li className="ArticleListItem">
-                <a href="/" target="_blank">
-                    <h3 className="ArticleListItem-title">Thinking in React</h3>
-                </a>
-                <div className="AuthorInfo">
-                    <span className="AuthorInfo-avatar">
-                        <a className="UserLink-link" target="_blank" href="/">
-                            <img width="24" height="24" src="" alt="" />
-                        </a>
-                    </span>
-                    <span className="AuthorInfo-name">
-                        <a target="_blank" href="/">Reactjs</a>
-                    </span>
-                    <span className="AuthorInfo-date">
-                        <time dateTime="1559750400000">06 June 2019</time>
-                    </span>
+        <li className="ArticleItem">
+            <a className="ArticleItem-title" href="/" target="_blank">
+                <h3>Thinking in React</h3>
+            </a>
+            <div className="AuthorInfo">
+                <span className="AuthorInfo-avatar">
+                    <a target="_blank" href="/">
+                        <img width="24" height="24" src="" alt="" />
+                    </a>
+                </span>
+                <span className="AuthorInfo-name">
+                    <a target="_blank" href="/">Reactjs</a>
+                </span>
+                <span className="AuthorInfo-date">
+                    <time dateTime="1559750400000">06 June 2019</time>
+                </span>
+            </div>
+            <div className="ArticleItem-excerpt">
+                <div className="ArticleItem-cover">
+                    <img src="" alt="cover" />
                 </div>
-                <a className="ArticleListItem-Excerpt" href="/" target="_blank">
-                    <div className="ArticleListItem-cover">
-                        <img src="" alt="cover" />
-                    </div>
-                    <p className="ArticleListItem-inner">One of the many great parts of React is how it makes you think about apps as you build them. In this document, we’ll walk you through the thought process of building a searchable product data table using React.
-                            <small className="ArticleItem-Excerpt-more">More</small>
-                    </p>
-                </a>
-                <div className="ArticleListItem-actions">
-                    <button>Vote</button>
-                    <button>Comment</button>
-                </div>
-            </li >
-        </div >
+                <p className="ArticleItem-inner">
+                    One of the many great parts of React is how it makes you think about apps as you build them. In this document, we’ll walk you through the thought process of building a searchable product data table using React.
+                    <small className="ArticleItem-Excerpt-more">More</small>
+                </p>
+            </div>
+            <div className="ArticleItem-actions">
+                <button>Vote</button>
+                <button>Comment</button>
+            </div>
+        </li >
     );
 }
 
@@ -44,8 +44,15 @@ export class ArticleList extends React.Component {
     render() {
         return (
             <section className="ArticleList">
-                <h2 className="ArticleList-title">Latest</h2>
-                <ArticleListItem />
+                <nav className="ArticleList-nav">
+                    <a href="/">Remo</a>
+                    <a href="/">Latest</a>
+                    <a href="/">Hot</a>
+                </nav>
+                <ul className="ArticleList-items">
+                    {[0, 1, 2, 3, 4, 5].map((value, index) => <ArticleItem key={index} />)}
+
+                </ul>
             </section >
         );
     }
--- src/components/StartBar/index.js
+++ src/components/StartBar/index.js
@@ -4,7 +4,7 @@ import './style.css';
 
 const StartBar = (props) =>
     (<h3 className="StartBar">
-        <div className="StartBar-text">{props.text}</div>
+        {props.text ? <div className="StartBar-text">{props.text}</div> : null}
         <div className="StartBar-line"></div>
     </h3>)
 
