ace4d11a182874f2a02e17d68aacb226a698e0b3
Blunt <zhgy@hotmail.com>
2019-10-06 16:54:18 +0800

    Added ProfileCard.

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { Banner } from './Banner';
 export { CardContainer } from './CardContainer';
 export { RichText } from './RichText';
 export { StartBar } from './StartBar';
+export { ProfileCard } from './ProfileCard';
